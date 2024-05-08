99004c64ca1b9e3333cbb49996eab6ad404a4d9c
Gowtham1802 <gowthama7575@gmail.com>
2024-05-08 18:08:15 +0530

    add last update codes

--- src/components/FormInputScreen.js
+++ src/components/FormInputScreen.js
@@ -18,11 +18,25 @@ const FormInputScreen = ({navigation, route}) => {
     address: '',
   });
 
-  useEffect(() => {
-    if (route.params && route.params.studentData) {
-      setFormData(route.params.studentData);
-    }
-  }, [route.params]);
+   useEffect(() => {
+     if (route.params && route.params.studentData) {
+       setFormData(route.params.studentData);
+     } else {
+       // Clear form data when adding new entry
+       setFormData({
+         name: '',
+         dateOfBirth: '',
+         mobileNumber: '',
+         fathersName: '',
+         fathersOccupation: '',
+         mothersName: '',
+         mothersOccupation: '',
+         selectedClass: '',
+         selectedGender: '',
+         address: '',
+       });
+     }
+   }, [route.params]);
 
   const handleChange = (key, value) => {
     setFormData({
@@ -32,34 +46,90 @@ const FormInputScreen = ({navigation, route}) => {
   };
 
   const handleSubmit = async () => {
-    try {
-      const existingStudentData = await AsyncStorage.getItem('studentData');
-      let studentData = existingStudentData
-        ? JSON.parse(existingStudentData)
-        : [];
+    const errors = handleValidation();
+    if (Object.keys(errors).length === 0) {
+      try {
+        const existingStudentData = await AsyncStorage.getItem('studentData');
+        let studentData = existingStudentData
+          ? JSON.parse(existingStudentData)
+          : [];
 
-      if (route.params && route.params.studentIndex !== undefined) {
-        // If updating existing data, replace the data at the specified index
-        studentData[route.params.studentIndex] = formData;
-      } else {
-        // If adding new data, push it to the array
-        studentData.push(formData);
+        if (route.params && route.params.studentIndex !== undefined) {
+          // If updating existing data, replace the data at the specified index
+          studentData[route.params.studentIndex] = formData;
+        } else {
+          // If adding new data, push it to the array
+          studentData.push(formData);
+        }
+
+        await AsyncStorage.setItem('studentData', JSON.stringify(studentData));
+        navigation.navigate('FormDataScreen');
+      } catch (error) {
+        console.error('Error saving form data: ', error);
       }
-
-      await AsyncStorage.setItem('studentData', JSON.stringify(studentData));
-      navigation.navigate('FormDataScreen');
-    } catch (error) {
-      console.error('Error saving form data: ', error);
+    } else {
+      Alert.alert('Validation Error', Object.values(errors).join('\n'));
     }
   };
 
   const handleGetData = async () => {
     try {
       const existingStudentData = await AsyncStorage.getItem('studentData');
-      alert(existingStudentData ? existingStudentData : 'No data found');
+      if (existingStudentData) {
+        const lastEntry = JSON.parse(existingStudentData).pop(); // Get the last entered data
+        if (lastEntry) {
+          setFormData(lastEntry);
+        }
+      } else {
+        alert('No data found');
+      }
     } catch (error) {
       console.error('Error getting form data: ', error);
     }
+  };
+
+  const handleValidation = () => {
+    const errors = {};
+
+    if (!formData.name) {
+      errors.name = 'Name is required';
+    }
+    // Add validations for other fields...
+    if (!formData.dateOfBirth) {
+      errors.dateOfBirth = 'Date of Birth is required';
+    }
+
+    if (!formData.mobileNumber) {
+      errors.mobileNumber = 'Mobile Number is required';
+    } else if (!/^\d{10}$/.test(formData.mobileNumber)) {
+      errors.mobileNumber = 'Mobile Number must be 10 digits';
+    }
+
+    if (!formData.selectedGender) {
+      errors.selectedGender = 'Gender is required';
+    }
+
+    if (!formData.fathersName) {
+      errors.fathersName = "Father's Name is required";
+    }
+
+    if (!formData.fathersOccupation) {
+      errors.fathersOccupation = "Father's Occupation is required";
+    }
+
+    if (!formData.mothersName) {
+      errors.mothersName = "Mother's Name is required";
+    }
+
+    if (!formData.mothersOccupation) {
+      errors.mothersOccupation = "Mother's Occupation is required";
+    }
+
+    if (!formData.address) {
+      errors.address = 'Address is required';
+    }
+
+    return errors;
   };
 
   return (
